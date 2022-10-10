7a201a7e56511f05002b0f2cb3ec6f3a608bb722
iliamiltykh <81648138+iliamiltykh@users.noreply.github.com>
2022-10-10 19:01:39 +0300

    change to omogenis.ru

--- omogenis/docusaurus.config.js
+++ omogenis/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'ομογενής',
   tagline: 'Греческие соотечественники в странах бывшего Советсткого союза',
-  url: 'https://omogenis.github.io',
+  url: 'https://omogenis.ru',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
