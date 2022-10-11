d841505ed27dc3cdc90edfa16b27f8926501e6dd
iliamiltykh <81648138+iliamiltykh@users.noreply.github.com>
2022-10-12 01:57:04 +0300

    FIX: edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/omogenis/omogenis.github.io/',
+            'https://github.com/omogenis/omogenis.github.io/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/omogenis/omogenis.github.io/',
+            'https://github.com/omogenis/omogenis.github.io/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
