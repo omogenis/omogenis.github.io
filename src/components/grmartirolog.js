19cb6e09529ef3811cae0afdb3a26ef9a5fc4235
Ilia Miltykh <81648138+iliamiltykh@users.noreply.github.com>
2024-03-31 10:57:15 +0300

    Update grmartirolog.js

--- src/components/grmartirolog.js
+++ src/components/grmartirolog.js
@@ -9,7 +9,7 @@ function Contacts() {
 			<div className="flex flex-col" bis_skin_checked="1">
 				<h4 className="text-3xl font-bold">Иван Георгиевич Джуха</h4>
 				<p className="dark:text-gray-800">C 1977 занимается изучением истории своего народа. С 2004 И. Джуха — руководитель проекта «Греческий мартиролог». Целью проекта является комплексное исследование репрессий в отношении греческого народа в СССР, сохранение памяти о погибших и пострадавших людях. За реализацию проекта «Греческий мартиролог» И. Г. Джуха в 2006 награждён золотой медалью мэрии города Салоники «За вклад в развитие эллинизма»</p>
-        {/* <a className="button button--primary button--lg" href="http://www.greek-martirolog.ru/"> Сайт проекта  </a> */}
+        			<a className="button button--primary button--lg" href="http://www.greek-martirolog.com/"> Сайт проекта  </a>
 			</div>
 		</div>
     </div>
